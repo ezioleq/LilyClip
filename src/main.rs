db9ea47b8f15d4a04de2d85a25dcfcd684649944
Ezioleq <31481375+Ezioleq@users.noreply.github.com>
2021-12-12 11:58:17 +0100

    Move UI creation to separate function

--- src/main.rs
+++ src/main.rs
@@ -20,9 +20,9 @@ fn add_header_button_popover(button: &gtk::MenuButton) {
             .title(strings::TITLE)
             .version(strings::VERSION)
             .license_type(gtk::License::Gpl30)
-            .authors(vec!(strings::AUTHOR.into()))
+            .authors(vec![strings::AUTHOR.into()])
             .build();
-        
+
         about.show_all();
     });
 
@@ -43,44 +43,44 @@ fn add_header_button_popover(button: &gtk::MenuButton) {
     button.set_popover(Some(&popover));
 }
 
+fn build_ui(app: &Application) {
+    let win = ApplicationWindow::builder()
+        .application(app)
+        .default_width(350)
+        .default_height(400)
+        .title(strings::TITLE)
+        .build();
+
+    // Header bar button for misc options
+    let csd_button = gtk::MenuButton::new();
+    let csd_button_img = gtk::Image::new();
+    csd_button_img.set_from_icon_name("open-menu-symbolic".into(), gtk::IconSize::Button);
+    csd_button.set_image(Some(&csd_button_img));
+
+    // Popover for header bar button
+    add_header_button_popover(&csd_button);
+
+    // CSD header bar
+    let header = gtk::HeaderBar::builder()
+        .title(strings::TITLE)
+        .subtitle(strings::SUBTITLE)
+        .has_subtitle(false)
+        .show_close_button(true)
+        .build();
+
+    // Add and set the new titlebar
+    header.add(&csd_button);
+    win.set_titlebar(Some(&header));
+
+    win.show_all();
+}
+
 fn main() {
     let app = Application::builder()
         .application_id("com.ezioleq.LilyClip")
         .build();
 
-    app.connect_activate(|app| {
-        let win = ApplicationWindow::builder()
-            .application(app)
-            .default_width(350)
-            .default_height(400)
-            .title(strings::TITLE)
-            .build();
-
-        // Header bar button for misc options
-        let csd_button = gtk::MenuButton::new();
-        let csd_button_img = gtk::Image::new();
-        csd_button_img.set_from_icon_name(
-            "open-menu-symbolic".into(),
-            gtk::IconSize::Button
-        );
-        csd_button.set_image(Some(&csd_button_img));
-
-        // Popover for header bar button
-        add_header_button_popover(&csd_button);
-        
-        // CSD header bar
-        let header = gtk::HeaderBar::builder()
-            .title(strings::TITLE)
-            .subtitle(strings::SUBTITLE)
-            .has_subtitle(false)
-            .show_close_button(true)
-            .build();
-
-        header.add(&csd_button);
-
-        win.set_titlebar(Some(&header));
-        win.show_all();
-    });
+    app.connect_activate(build_ui);
 
     app.run();
 }
