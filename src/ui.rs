bb655ec3fe7b4c26216ed1f89a2c135b9a31fcc8
Ezioleq <31481375+Ezioleq@users.noreply.github.com>
2021-12-12 18:06:56 +0100

    Add ui module documentation

--- src/ui.rs
+++ src/ui.rs
@@ -1,9 +1,13 @@
+//! Set of some utility functions for creating UI elements
+
 use gtk::prelude::*;
 
+/// Create an [Button](gtk::Button) with an icon of the given name
 pub fn create_button_with_icon(icon_name: &str) -> gtk::Button {
 	gtk::Button::from_icon_name(icon_name.into(), gtk::IconSize::Button)
 }
 
+/// Create a [MenuButton](gtk::MenuButton) with an icon of the given name
 pub fn create_menu_button_with_icon(icon_name: &str) -> gtk::MenuButton {
     let menu_button = gtk::MenuButton::new();
     let img = gtk::Image::new();
