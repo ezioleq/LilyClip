e57fabce9230fcf573c2b99b24249560785b445b
Ezioleq <31481375+Ezioleq@users.noreply.github.com>
2021-12-12 12:45:26 +0100

    Add strings module documentation

--- src/strings.rs
+++ src/strings.rs
@@ -1,4 +1,12 @@
+//! Some constraints used in the program
+
+/// Application title
 pub const TITLE: &str = "LilyClip";
+/// Header bar's subtitle
 pub const SUBTITLE: &str = "Your frequently used phrases";
+/// Author of the application (used in About dialog)
 pub const AUTHOR: &str = "Ezioleq";
+/// Application version string
+///
+/// Uses environment variable CARGO_PKG_VERSION which is passed by cargo
 pub const VERSION: &'static str = env!("CARGO_PKG_VERSION");
