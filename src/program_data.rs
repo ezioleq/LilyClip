933179a1c2e5bc8f1df64e8a8a16f9c297f14e0f
Ezioleq <31481375+Ezioleq@users.noreply.github.com>
2021-12-18 16:45:37 +0100

    Program data module docs

--- src/program_data.rs
+++ src/program_data.rs
@@ -1,12 +1,25 @@
+//! Used to manage the program data in the filesystem
+
 use directories::ProjectDirs;
 use std::fs::File;
 use std::io::BufRead;
 
+/// Program data manager
 pub struct ProgramData {
+    /// Path to the program data directory
     pub path: std::path::PathBuf,
 }
 
 impl ProgramData {
+    /// Creates a new program data
+    ///
+    /// It also handles program data directory creation if it doesn't exist.  
+    /// Created directory is located at:
+    /// ```
+    /// Linux: $HOME/.local/share
+    /// Windows: {FOLDERID_RoamingAppData}
+    /// MacOS: $HOME/Library/ApplicationSupport
+    /// ```
     pub fn new() -> ProgramData {
         let dir = match ProjectDirs::from("com", "Ezioleq", "LilyClip") {
             Some(dir) => dir,
@@ -25,11 +38,11 @@ impl ProgramData {
         ProgramData { path: dir }
     }
 
+    /// Get all saved lines from the file at the given path
     pub fn get_saved_lines(&self, filename: &str) -> Vec<String> {
-        let file = File::open(self.path.join(filename))
-            .expect("Saved data file is missing");
+        let file = File::open(self.path.join(filename)).expect("Saved data file is missing");
 
-		let buf = std::io::BufReader::new(file);
-		buf.lines().filter_map(std::io::Result::ok).collect()
+        let buf = std::io::BufReader::new(file);
+        buf.lines().filter_map(std::io::Result::ok).collect()
     }
 }
